c27f0f7e0f7be5976ae3a6781e4859f0e87fd977
Kukuh Ardi Ramadhan <kukuhardi669@gmail.com>
2025-06-07 19:55:15 +0700

    [feature] search recipe

--- server/api/recipe/public-recipe.get.ts
+++ server/api/recipe/public-recipe.get.ts
@@ -1,23 +1,39 @@
 import { useDb } from '~/server/utils/useDb'
+import { Recipe } from '~/utils/zod/recipeSchema'
 
 export default defineEventHandler(async (event) => {
-    try {
-        const { db } = useDb(event)
-        const recipeSnap = await db.collection('public_recipes').get()
-        const recipesData = recipeSnap.docs.map((recipe) => ({ id: recipe.id, ...recipe.data() }))
+  try {
+    const { db } = useDb(event)
+    const recipeCollection = db.collection('public_recipes')
+    const allRecipeSnap = await recipeCollection.get()
+    const recipesData = allRecipeSnap.docs.map((recipe) => ({
+      id: recipe.id,
+      ...recipe.data()
+    })) as Recipe[]
 
-        return {
-            success: true,
-            data: recipesData,
-            message: "Success get all recipes"
-        }
-    } catch (err) {
-        const { message, cause } = err as Error
-        throw createError({
-            statusCode: 500,
-            statusMessage: "Internal server error",
-            message,
-            cause
-        })
+    const query = getQuery(event)
+    const recipeQueryTitle = query.title?.toString().trim().toLowerCase()
+
+    const filteredRecipes = recipeQueryTitle
+      ? recipesData.filter((e) =>
+        e.title.toLowerCase().includes(recipeQueryTitle)
+      )
+      : recipesData
+
+    return {
+      success: true,
+      data: filteredRecipes,
+      message: recipeQueryTitle
+        ? `Filtered recipes by title: "${recipeQueryTitle}"`
+        : "Success get all recipes"
     }
+  } catch (err) {
+    const { message, cause } = err as Error
+    throw createError({
+      statusCode: 500,
+      statusMessage: "Internal server error",
+      message,
+      cause
+    })
+  }
 })
