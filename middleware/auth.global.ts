b490de7adcea13a2a662d7b7aeb404ee111b9741
Kukuh Ardi Ramadhan <kukuhardi669@gmail.com>
2025-07-03 13:23:26 +0700

    [fix] fix redirect error

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,9 +1,13 @@
 export default defineNuxtRouteMiddleware(async (to) => {
-    const cookie = useCookie("firebase_access_token").value
-    console.log({ cookie })
+    const headers = useRequestHeaders()
+    const cookieHeader = headers.cookie || ""
+    const cookie = cookieHeader.match(/firebase_access_token=([^;]+)/)?.[1] || null
     const protectedPage = to.path.startsWith("/profile") || to.path.startsWith("/new-recipe")
+    const authPage = to.path === "/login" || to.path === "/register"
 
-    if (!cookie && protectedPage) {
-       return navigateTo("/login")
+    if (cookie && authPage) {
+        return navigateTo("/")
+    } else if (!cookie && protectedPage) {
+        return navigateTo("/login")
     }
 })
