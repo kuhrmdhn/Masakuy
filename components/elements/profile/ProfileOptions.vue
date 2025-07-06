95bc2f2d19a693abde9702e1d1ab3e98a5bc69cd
Kukuh Ardi Ramadhan <kukuhardi669@gmail.com>
2025-07-06 20:20:35 +0700

    [feature] profile options button

---
+++ components/elements/profile/ProfileOptions.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import LogoutButton from "./LogoutButton.vue";
+</script>
+
+<template>
+  <DropdownMenu>
+    <DropdownMenuTrigger>
+      <Button variant="ghost">
+        <Icon name="iwwa:option-horizontal" class="text-2xl" />
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent class="h-full w-fit flex flex-col gap-2 items-center py-2">
+      <NuxtLink href="/profile/edit">
+        <Button variant="secondary" class="font-semibold">Edit Profil</Button>
+      </NuxtLink>
+      <LogoutButton />
+    </DropdownMenuContent>
+  </DropdownMenu>
+</template>
