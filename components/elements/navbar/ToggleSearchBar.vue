926b41529ee2e3118088443f0042bec4ae7fc213
Kukuh Ardi Ramadhan <kukuhardi669@gmail.com>
2025-07-22 19:26:31 +0700

    [utils] add search button trigger name & fix empty recipe image style

--- components/elements/navbar/ToggleSearchBar.vue
+++ components/elements/navbar/ToggleSearchBar.vue
@@ -1,15 +1,17 @@
 <script lang="ts" setup>
-const isShowSearchBar = useState("is-show-search-bar", () => false)
+const isShowSearchBar = useState("is-show-search-bar", () => false);
 
 function toggleSearchBar() {
-    isShowSearchBar.value = !isShowSearchBar.value
+  isShowSearchBar.value = !isShowSearchBar.value;
 }
 
-const icons = computed(() => isShowSearchBar.value ? 'mingcute:close-fill' : 'ic:twotone-search')
+const icons = computed(() =>
+  isShowSearchBar.value ? "mingcute:close-fill" : "ic:twotone-search"
+);
 </script>
 
 <template>
-  <Button variant="ghost" @click="toggleSearchBar">
+  <Button aria-label="Search Button Trigger" variant="ghost" @click="toggleSearchBar">
     <Icon :name="icons" class="text-xl" />
   </Button>
 </template>
